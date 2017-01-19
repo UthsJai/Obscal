63861ac1b9c59b889bf6f3e592831fd94f39948d
MERC-LAB-01 <MERC-LAB-01@MERC-LAB-02>
2017-01-19 14:30:59 +0530

    Initial Commit

---
+++ src/poacalculator/PoaCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poacalculator;
+
+/**
+ *
+ * @author USER
+ */
+public class PoaCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Poa object = new Poa ();
+         object.setVisible(true);
+    }
+    
+}
